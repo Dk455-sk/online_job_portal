f46e459f38b75402a03b799cd7f0fefdae8e08d2
Dk455-sk <kapildevsurujal8@gmail.com>
2026-03-07 12:47:12 +0530

    fix database config

--- online_job_portal/settings.py
+++ online_job_portal/settings.py
@@ -87,7 +87,8 @@ WSGI_APPLICATION = 'online_job_portal.wsgi.application'
 # }
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600
     )
 }
 
